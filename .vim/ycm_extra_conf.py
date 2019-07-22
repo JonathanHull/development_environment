f54cdec7fdf030ddaa8f6d21f65e949c9844d5df
Jonathan Hull <jonathan.hull11@gmail.com>
2019-07-22 11:32:31 +0100

    Added clang includes, go syntax hightlighting in ycm_extra_conf.py

--- .vim/ycm_extra_conf.py
+++ .vim/ycm_extra_conf.py
@@ -10,6 +10,12 @@ flags = [
   '-Wno-variadic-macros',
   '-fexceptions',
   '-ferror-limit=10000',
+  '-x', 'c++',
+  '-isystem', '/usr/include/c++/7.2.1',
+  '-isystem', '/usr/include/c++/7.2.1/x86_64-pc-linux-gnu',
+  '-isystem', '/usr/include/c++/7.2.1/backward',
+  '-isystem', '/usr/local/include',
+  '-isystem', '/usr/include',
   #'-DNDEBUG',
   #'-std=c99',
   #'-xc',
@@ -23,3 +29,15 @@ def FlagsForFile( filename, **kwargs ):
   'flags': flags,
   'do_cache': True
   }
+
+def Settings(**kwargs):
+    if kwargs['language'] == 'go':
+        return {"language_server": [ {
+            "name": "gopls",
+            "cmdline": ["/home/jonathan/go/bin/gopls", "-rpc.trance" ],
+            "filetypes": ["go"],
+            "project_root_files": ["go.mod"]
+            } ]
+        }
+
+
